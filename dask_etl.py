cd59c77ccfb4cf87a55b36f3cd0baf7e1eaeea7e
porbmv83 <93665757+porbmv83@users.noreply.github.com>
2021-11-15 13:08:04 +0100

    Update dask_etl.py

--- dask_etl.py
+++ dask_etl.py
@@ -3,7 +3,7 @@ import datetime
 import random
 from prefect.storage import GitHub
 from prefect.run_configs import KubernetesRun
-from prefect.engine.executors import DaskExecutor
+from prefect.environments import DaskKubernetesEnvironment
 
 FLOW_NAME = "dask_etl"
 STORAGE = GitHub(
@@ -11,8 +11,6 @@ STORAGE = GitHub(
     path=f"{FLOW_NAME}.py",
     #access_token_secret="GITHUB_ACCESS_TOKEN",   required with private repositories
 )
-
-executor = DaskExecutor(address="20.85.133.123:8786")
 
 @task
 def inc(x):
@@ -37,4 +35,4 @@ with Flow(FLOW_NAME, storage=STORAGE, run_config=KubernetesRun(labels=["porbmv"]
     adds = add.map(x=incs, y=decs)
     total = list_sum(adds)
 
-flow.run(executor=executor)
+flow.environment = DaskKubernetesEnvironment()
