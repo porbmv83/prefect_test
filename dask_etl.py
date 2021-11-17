9eaa3b4b5f62537b615f3f30db2ab2cab7ab0554
porbmv83 <93665757+porbmv83@users.noreply.github.com>
2021-11-17 10:00:45 +0100

    Update dask_etl.py

--- dask_etl.py
+++ dask_etl.py
@@ -30,9 +30,11 @@ def add(x, y):
 def list_sum(arr):
     return sum(arr)
 
-with Flow(FLOW_NAME, storage=STORAGE, run_config=KubernetesRun(labels=["porbmv"],), executor = DaskExecutor(
-    cluster_class="dask_cloudprovider.azure.AzureVMCluster",
-    adapt_kwargs={"maximum": 4},)) as flow:
+with Flow(FLOW_NAME, 
+    storage=STORAGE, 
+    run_config=KubernetesRun(env={"EXTRA_PIP_PACKAGES": "prefect" "git+https://github.com/dask/distributed"} labels=["porbmv"],), 
+    executor = DaskExecutor(cluster_class="dask_cloudprovider.azure.AzureVMCluster", adapt_kwargs={"maximum": 4},)
+    ) as flow:
     incs = inc.map(x=range(100))
     decs = dec.map(x=range(100))
     adds = add.map(x=incs, y=decs)
