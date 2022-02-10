b5d3e0634fa7103fa432271fb41b1be239305584
EUROPE\porbmv <bruno.nunes@sas.com>
2022-02-10 14:33:50 +0100

    Test

--- wo_dask_etl.py
+++ wo_dask_etl.py
@@ -11,26 +11,27 @@ FLOW_NAME = "wo_dask_etl"
 STORAGE = GitHub(
     repo="porbmv83/prefect_test",
     path=f"{FLOW_NAME}.py",
-    #access_token_secret="GITHUB_ACCESS_TOKEN",   required with private repositories
+    # access_token_secret="GITHUB_ACCESS_TOKEN",   required with private repositories
 )
 
 POD_SPEC = make_pod_spec(
-                         image="prefecthq/prefect:0.15.9",
-                         memory_limit="2G", 
-                         memory_request="2G",
-                         cpu_limit=1, 
-                         cpu_request=1,
-                         env={"EXTRA_PIP_PACKAGES": "prefect fastparquet distributed", 
-                                "TZ": "Europe/Amsterdam"},)
+    image="prefecthq/prefect:0.15.9",
+    memory_limit="2G",
+    memory_request="2G",
+    cpu_limit=1,
+    cpu_request=1,
+    env={"EXTRA_PIP_PACKAGES": "prefect fastparquet distributed",
+         "TZ": "Europe/Amsterdam"},)
 
 EXECUTOR = DaskExecutor(
     cluster_class="dask_kubernetes.KubeCluster",
     cluster_kwargs={"pod_template": POD_SPEC,
                     "name": "my-dask-pod"
                     },
-    adapt_kwargs={"minimum": 1, 
+    adapt_kwargs={"minimum": 1,
                   "maximum": 2},
 )
+
 
 @task
 def inc(x):
@@ -41,18 +42,22 @@ def inc(x):
 def dec(x):
     return x - 1
 
+
 @task
 def add(x, y):
     return x + y
+
 
 @task
 def list_sum(arr):
     return sum(arr)
 
-with Flow(FLOW_NAME, 
-        storage=STORAGE,
-        run_config=KubernetesRun(env={"EXTRA_PIP_PACKAGES": "prefect dask distributed dask-kubernetes"}, labels=["porbmv"],),  
-        executor = EXECUTOR,) as flow:
+
+with Flow(FLOW_NAME,
+          storage=STORAGE,
+          run_config=KubernetesRun(
+              env={"EXTRA_PIP_PACKAGES": "prefect dask distributed dask-kubernetes"}, labels=["porbmv"],),
+          executor=EXECUTOR,) as flow:
     incs = inc.map(x=range(100))
     decs = dec.map(x=range(100))
     adds = add.map(x=incs, y=decs)
