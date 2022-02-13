a3377d40ac6206ac3aaed2063ab94fa9a5b66f9e
EUROPE\porbmv <bruno.nunes@sas.com>
2022-02-13 17:13:40 +0100

    Test

--- dask_spre.py
+++ dask_spre.py
@@ -112,7 +112,7 @@ with Flow(FLOW_NAME,
           storage=STORAGE,
           run_config=RUN_CONFIG,
           executor=EXECUTOR,) as flow:
-    incs = inc.map(x=range(100))
-    decs = dec.map(x=range(100))
+    incs = inc.map(x=range(10))
+    decs = dec.map(x=range(10))
     adds = add.map(x=incs, y=decs)
     total = list_sum(adds)
