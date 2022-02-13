5680b3d8d76b69d4d64be2820cc56715b997ff26
EUROPE\porbmv <bruno.nunes@sas.com>
2022-02-13 13:03:06 +0100

    test

--- dask_spre.py
+++ dask_spre.py
@@ -10,8 +10,83 @@ STORAGE = GitHub(
     path=f"{FLOW_NAME}.py",
     # access_token_secret="GITHUB_ACCESS_TOKEN",   required with private repositories
 )
+
+
+worker_spec = {
+    "kind": "Pod",
+    "metadata": {
+        "labels": {
+            "app": "prefect-spre"
+        }
+    },
+    "spec": {
+        "volumes": [
+            {
+                "name": "core",
+                "persistentVolumeClaim": {
+                    "claimName": "sas-risk-cirrus-core-pvc"
+                }
+            }
+        ],
+        "restartPolicy": "Never",
+        "containers": [
+            {
+                "image": "sasporbmvacr.azurecr.io/prefect-dask-spre:latest",
+                "imagePullPolicy": "IfNotPresent",
+                "args": [
+                    "dask-worker",
+                    "$(DASK_SCHEDULER_ADDRESS)",
+                    "--nthreads",
+                    "2",
+                    "--memory-limit",
+                    "2GB",
+                    "--no-dashboard",
+                    "--death-timeout",
+                    "60"
+                ],
+                "name": "dask-worker",
+                "env": {
+                    "resources": {
+                        "limits": {
+                            "cpu": "2",
+                            "memory": "2G"
+                        },
+                        "requests": {
+                            "cpu": "2",
+                            "memory": "1G"
+                        }
+                    }
+                },
+                "volumeMounts": [
+                    {
+                        "name": "core",
+                        "mountPath": "/core"
+                    }
+                ]
+            }
+        ],
+        "imagePullSecrets": [
+            {
+                "name": "sasporbmvacr-image-pull-secret"
+            }
+        ],
+        "nodeSelector": {
+            "workload.sas.com/class": "compute"
+        },
+        "tolerations": [
+            {
+                "effect": "NoSchedule",
+                "key": "workload.sas.com/class",
+                "operator": "Equal",
+                "value": "compute"
+            }
+        ]
+    }
+}
+
+
 EXECUTOR = DaskExecutor(
-    cluster_class=lambda: KubeCluster("worker-spec.yaml"),
+    cluster_class=lambda: KubeCluster(worker_spec),
     adapt_kwargs={"minimum": 1, "maximum": 2},
 )
 
