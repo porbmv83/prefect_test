7fb36562d956bae65afd28a420c5915da2a1253f
EUROPE\porbmv <bruno.nunes@sas.com>
2022-02-17 18:19:40 +0100

    test

--- prefect_data_prep_local.py
+++ prefect_data_prep_local.py
@@ -2,33 +2,39 @@ from prefect import Flow, Task, Parameter
 import saspy
 import os
 import time
+import prefect
 
 
 class RunSpreTask(Task):
-    def run(self, args):
-        # Collecting the parameters needed from the list of dictionaries
-        dict_fa_path = next(
-            (item for item in args if item['NAME'] == 'FA_PATH'), None)
-        dict_run_instance = next(
-            (item for item in args if item['NAME'] == 'RUN_INSTANCE'), None)
-        dict_node_code = next(
-            (item for item in args if item['NAME'] == 'NODE_CODE'), None)
+    def run(self, **params):
+        # Get the values from the global parameters
+        FA_PATH = prefect.context.parameters['FA_PATH']
+        RUN_INSTANCE = prefect.context.parameters['RUN_INSTANCE']
+
+        # Prepare the JSON string to pass to core_run_sas
+        list_params = []
+        for key, value in params.items():
+            dict_param = dict()
+            dict_param["NAME"] = key
+            dict_param["VALUE"] = value
+            list_params.append(dict_param)
 
         # Create a SAS session and run the core_run_sas.sas wrapper passing the parameters
         sas = saspy.SASsession()
-        sas.symput('FA_PATH', dict_fa_path['VALUE'])
-        sas.symput('INPUT_PARAMETERS', args)
+        sas.symput('FA_PATH', FA_PATH)
+        sas.symput('RUN_INSTANCE', RUN_INSTANCE)
+        sas.symput('INPUT_PARAMETERS', list_params)
         r = sas.submit('''
-			%put : INPUT_PARAMETERS: &INPUT_PARAMETERS;
+			%put INPUT_PARAMETERS: &INPUT_PARAMETERS;
 			%include "&FA_PATH\prefect\source\core_run_sas.sas"; 
 		''')
 
         # Show the log and save it in a file
         print(r['LOG'])
-        log_name = dict_node_code['VALUE'] + "_" + \
+        log_name = params['NODE_CODE'] + "_" + \
             time.strftime("%Y%m%d_%H%M%S") + '.log'
         log_file = open(os.path.join(
-            dict_run_instance['VALUE']+'\\logs', log_name), "w")
+            RUN_INSTANCE+'\\logs', log_name), "w")
         log_file.write(r['LOG'])
         log_file.close()
 
@@ -36,44 +42,41 @@ class RunSpreTask(Task):
         sas.endsas()
 
 
-def processParameters(**args):
-    # Function to create a list of dict pair name and value
-    list_args = []
-    for key, value in args.items():
-        dict_arg = dict()
-        dict_arg["NAME"] = key
-        dict_arg["VALUE"] = value
-        list_args.append(dict_arg)
-
-    return list_args
-
-
 # Instantiate the flow and the task
 initialize = RunSpreTask(name='Initialize', log_stdout=True)
+initialize2 = RunSpreTask(name='Initialize2', log_stdout=True)
+flow = Flow("prefect-spre-dataPrep")
 
-with Flow("prefect-spre-dataPrep") as flow:
+# Global parameters
+BASE_DT = Parameter('BASE_DT', default='12312019')
+ENTITY_ID = Parameter('ENTITY_ID', default='SASBank_1')
+CYCLE_ID = Parameter('CYCLE_ID', default='10000')
+FA_ID = Parameter('FA_ID', default='2022.1.1')
+FA_PATH = Parameter(
+    'FA_PATH', default='C:\\Development\\Temp\\Prefect_Cirrus_Core\\core\\2022.1.1\\')
+RUN_INSTANCE = Parameter(
+    'RUN_INSTANCE', default='C:\\Development\\Temp\\Prefect_Cirrus_Core\\core\\2022.1.1\\prefect\\run_instance\\prefect-spre-10000-12312019')
 
-    # Global parameters
-    BASE_DT = Parameter('BASE_DT', default='12312019')
-    ENTITY_ID = Parameter('ENTITY_ID', default='SASBank_1')
-    CYCLE_ID = Parameter('CYCLE_ID', default='10000')
-    FA_ID = Parameter('FA_ID', default='2022.1.1')
-    FA_PATH = 'C:\\Development\\Temp\\Prefect_Cirrus_Core\\core\\' + FA_ID + '\\'
-    RUN_INSTANCE = FA_PATH+'prefect\\run_instance\\' + \
-        'prefect-spre-'+CYCLE_ID+'-'+BASE_DT
+# Initialize and run the task
+flow.set_dependencies(task=initialize,
+                      upstream_tasks=[],
+                      keyword_tasks=dict(BASE_DT=BASE_DT,
+                                         CYCLE_ID=CYCLE_ID,
+                                         ENTITY_ID=ENTITY_ID,
+                                         FA_ID=FA_ID,
+                                         FA_PATH=FA_PATH,
+                                         RUN_INSTANCE=RUN_INSTANCE,
+                                         NODE_CODE='core_node_init',
+                                         RUN_OPTION='core_cfg.run_option',
+                                         SYSTEM_OPTION='sys_cfg.run_option',
+                                         FLOW_OPTION='core_res.flow_option')
+                      )
+flow.set_dependencies(task=initialize2,
+                      upstream_tasks=[initialize],
+                      keyword_tasks=dict(
+                          NODE_CODE='core_node_init2',
+                          FLOW_OPTION='core_res.flow_option')
+                      )
 
-    # Initialize and run the task
-    args = processParameters(BASE_DT=BASE_DT,
-                             CYCLE_ID=CYCLE_ID,
-                             ENTITY_ID=ENTITY_ID,
-                             FA_ID=FA_ID,
-                             FA_PATH=FA_PATH,
-                             RUN_INSTANCE=RUN_INSTANCE,
-                             NODE_CODE='core_node_init',
-                             RUN_OPTION='core_cfg.run_option',
-                             SYSTEM_OPTION='sys_cfg.run_option',
-                             FLOW_OPTION='core_res.flow_option'
-                             )
-    initialize(args)
-
+flow.visualize()
 flow.run()
