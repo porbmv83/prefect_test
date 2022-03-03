425103e772904d1040e0cdb8ee84bfd18154a630
Chi Nguyen <Chi.Nguyen@sas.com>
2022-03-03 14:48:13 -0500

    test

--- local_dask_compute_server.py
+++ local_dask_compute_server.py
@@ -71,8 +71,7 @@ def runSASCode(code, server, session_id, authheader):
 
     resp = requests.get(url=url, headers=authheader, verify=False)
     val = resp.json().get('items')[0].get('cells')[0]
-    print(code)
-    print("SAS code return value:" + str(val))
+    print("SAS code:" + code + " return value: "+ str(val))
     return val
 
 @task(log_stdout=True)
