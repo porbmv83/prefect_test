ea0be49313cc43863cfd1e0ebe4a645be4f70ec2
Chi Nguyen <Chi.Nguyen@sas.com>
2022-03-03 15:46:37 -0500

    test

--- local_dask_compute_server.py
+++ local_dask_compute_server.py
@@ -114,7 +114,7 @@ with Flow(FLOW_NAME,
     authheader = ''
     session_id, authheader = connectToComputeServer()
 
-    iterations = 1
+    iterations = 5
     servlist = [server] * iterations
     sessionlist = [session_id] * iterations
     authlist = [authheader] * iterations
