64861922a4cbc93032df7c22425b99fbf6b5ba47
EUROPE\porbmv <bruno.nunes@sas.com>
2022-03-24 11:03:54 +0100

    test

--- etl.py
+++ etl.py
@@ -6,8 +6,9 @@ FLOW_NAME = "etl"
 STORAGE = GitHub(
     repo="porbmv83/prefect_test",
     path=f"{FLOW_NAME}.py",
-    #access_token_secret="GITHUB_ACCESS_TOKEN",   required with private repositories
+    # access_token_secret="GITHUB_ACCESS_TOKEN",   required with private repositories
 )
+
 
 @task
 def extract():
@@ -18,9 +19,11 @@ def extract():
 def transform_1(x1):
     return [i * 10 for i in x1]
 
+
 @task
 def transform_2(x2):
     return [i * 10 for i in x2]
+
 
 @task
 def load(y, z):
